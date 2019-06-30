a34723928ffa89e561ad7e7085207a56277927b2
BergerProd <quentin.berge13@gmail.com>
2019-06-30 18:31:17 +0200

    Fin 1ère année

--- Matlab/EDO/EDO&EDPBERGE/Probleme3.m
+++ Matlab/EDO/EDO&EDPBERGE/Probleme3.m
@@ -1,19 +1,19 @@
 
 clear all
 close all
-
-
+t=10;
+tmp=0;
+C=zeros(1,t);
 nbr=9999;
-a=linspace(0,1,10000);
-y=1;
+y=linspace(0,1,10000);
 
-for t=0:10
+for t=1:10
         tmp = 0;
             for n=1:nbr
-        tmp = tmp + ((2*(-1)^n)*sin(n*pi*y)*exp(-n^2*pi^2*t))/(n*pi);
-        C(t+1)=tmp;   
+        tmp = tmp + ((2*(-1)^n)*sin(n*pi.*y)*exp(-n^2*pi^2*t))/(n*pi);
+          
             end
-       
+        C(t)=tmp;
     
     
 end
