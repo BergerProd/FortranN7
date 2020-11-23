7f9008754657953a34d056d0bea926b7e754d884
BergerProd <quentin.berge13@gmail.com>
2020-11-23 10:45:06 +0100

    DEV : working, mais pb de conditions limites et schema amont et non pas centré comme dans les consignes

--- Matlab/CLAT/script.m
+++ Matlab/CLAT/script.m
@@ -1,40 +1,41 @@
 %-------------------------------
-% Paramètres Entrée
+% Parametres Entree
 %-------------------------------
 %%% Maillage temporel et spatial
 nptz=200;
 L=1000; %m = H
 dt=0.2; %s
-tfinal=3600;
+tfinal=3600; %s
 %%% Conditions Initiales
-z0=1; %début : m
+z0=1; %debut du domaine : m
 L=L-z0;
 ug=10; %vitesse : m/s
 %%% Conditions Limites : cf fonction a la fin
 %%% Parametres
-rho=1; %masse volumique air supposée constante
+rho=1; %masse volumique air supposee constante
 K0=3; %
-kappa=0.41; %kappa du modèle de longueur de mélange
+kappa=0.41; %kappa du modele de longueur de melange
 f=1e-4; %s
-Gy=rho*ug*f;%9.81; %m/s^2
+Gy=rho*ug*f;
 lambda = 2.7*1e-4*ug/f;
 gamma=sqrt(f/(2*K0)); %parametre
 npastemps=(tfinal-0)/dt; %definition du pas de temps
 %-----------------------
 % Initialisation des vecteurs
+%-------------------------------
 k=zeros(1,nptz);
 k_bis=zeros(1,nptz);
 znoeuds=zeros(1,nptz);
 l=zeros(1,nptz);
-zcentre=zeros(1,nptz-1);
 u_l=zeros(1,nptz);
 v_l=zeros(1,nptz);
 K0_vect=K0*ones(nptz,1);
 u_t=zeros(npastemps,nptz);
 v_t=zeros(npastemps,nptz);
 
+%-------------------------------
 %% MAILLAGE
-%Pas dx
+%Pas dz
 dz = L/real(nptz-1);
 
 %initialisation des noeuds
@@ -44,58 +45,58 @@ znoeuds(1) = 0;
 for i=2:nptz
     znoeuds(i) = znoeuds(1)+dz*(i-1);
 end
-
-% %Centre des Volumes
-% %npt-1 sur les 2
-% zcentre(1)= znoeuds(1)+dz/2;
-% for i=2:nptz-1
-%     zcentre(i) = zcentre(1)+ dz*(i-1);
-% end    
-
-%Longueur de mélange
+%-------------------------------
+%Longueur de melange
 for i=1:nptz
     l(i)=(kappa*znoeuds(i))/(1+(kappa*znoeuds(i)/lambda));
 end
-
+%-------------------------------
 %determination u_l, v_l
 for i=2:nptz-1
     u_l(i)=ug*(1-exp(-gamma*znoeuds(i))*cos(gamma*znoeuds(i)));
     v_l(i)=ug*exp(-gamma*znoeuds(i))*sin(gamma*znoeuds(i));
-end    
+end
 
-% CL
+% appel a fonction CL
 [u_l,v_l]=CL(u_l,v_l,nptz,ug);
 
-% determination k (boucle ou diff) : donnent des résultats identiques
+%-------------------------------
+% determination k (boucle ou diff) : donnent des resultats identiques
 for i=1:nptz-1
     k(i)=(l(i)^2)*sqrt(((u_l(i+1)-u_l(i))/(znoeuds(i+1)-znoeuds(i)))^2 +((v_l(i+1)-v_l(i))/(znoeuds(i+1)-znoeuds(i)))^2);
-end 
+end
 k_bis=l(1:nptz-1).^2 .*sqrt( (diff(u_l)./diff(znoeuds)).^2+ (diff(v_l)./diff(znoeuds)).^2);
-        
 
+%diff est une fonction integrÃ©e a matlab qui pour un vecteur u renvoi la valeur de la diffÃ©rence de u(i+1)-u(i), elle se substitue donc a la notation lourde
+% diff(u) pour un vecteur de taille n renvoit une taille n-1, donc adaptation pour Ã©crire dans le vecteur k_bis qui devra pour tracer Ãªtre de la mÃªme taille que t
+
+%-------------------------------
+%Critere de convergence
 %definition dtbis, dt maximum pour convergence
 dtbis=(dz^2)/(max(k));
 %Si dt saisi trop important, message d'erreur
 if (dt>dtbis)
     disp('dt saisi = ',dt,'dt requis',dtbis);
     error('dt > dtbis');
-end   
-
+end
+%-------------------------------
 % calcul u_t et v_t
-%Remplissage premier temps
+%Remplissage premier temps qui seront donc les conditions initiales avec les vecteurs calcules precedemment u_l et v_l
 u_t(1,:)=u_l;
 v_t(1,:)=v_l;
 
 for t=1:npastemps-1 %boucle en temps
-    
+
     %1ere iteration
     if t==1
+        %vecteurs d2u qui servent a calculer la derivee seconde
+        %adaptation des tailles fonctions des pas de temps a parcourir puisque diff(u) renvoi un vecteur de taille n-1
         d2u=(diff(u_l)./diff(znoeuds)).*k(1:nptz-1);
         d2u=diff(d2u)./diff(znoeuds(1:nptz-1));
 
         d2v=(diff(v_l)./diff(znoeuds)).*k(1:nptz-1);
         d2v=diff(d2v)./diff(znoeuds(1:nptz-1));
-    
+
     else
         d2u=(diff(u_t(t,:))./diff(znoeuds)).*k(1:nptz-1);
         d2u=diff(d2u)./diff(znoeuds(1:nptz-1));
@@ -103,23 +104,23 @@ for t=1:npastemps-1 %boucle en temps
         d2v=(diff(v_t(t,:))./diff(znoeuds)).*k(1:nptz-1);
         d2v=diff(d2v)./diff(znoeuds(1:nptz-1));
     end
-    
+    %remplissage du pdt t+1 avec schema euler
     u_t(t+1,2:nptz-1)=u_t(t,2:nptz-1)+dt*(d2u+f.*v_t(t,2:nptz-1));
     v_t(t+1,2:nptz-1)=v_t(t,2:nptz-1)+dt*(d2v-f.*u_t(t,2:nptz-1)+Gy);
-    
+
     [u_t(t,:),v_t(t,:)]=CL(u_t(t,:),v_t(t,:),nptz,ug);%remplit 1 et nptz
 
-end    
-
+end
+%-------------------------------
 %% Sortie graphique
-
-% Longueur Mélange
+%-------------------------------
+% Longueur Melange
 figure(1)
 hold on
 plot(l,znoeuds)
 ylabel('$z$','Interpreter','Latex')
 xlabel('$l$','Interpreter','Latex')
-title('Longueur de mélange')
+title('Longueur de melange')
 grid on
 hold off
 
@@ -132,7 +133,7 @@ ylabel('$z$','Interpreter','Latex')
 xlabel('$u$,$v$','Interpreter','Latex')
 xlim([-2 inf])
 legend('u','v')
-title('Evolution des profils')
+title('Solutions stationnaires de u et v')
 grid on
 hold off
 
@@ -145,8 +146,8 @@ plot(K0_vect,znoeuds)
 plot(k_bis,znoeuds(1:nptz-1))
 ylabel('$z$','Interpreter','Latex')
 xlabel('$k$','Interpreter','Latex')
-legend('k','k0','k_{bis}')
-title('Evolution des profils')
+legend('k','k0','k_{bis} (methode diff)')
+title('Evolution des profils de K')
 grid on
 hold off
 
@@ -154,10 +155,9 @@ hold off
 figure(4)
 hold on
 plot(u_l,v_l)
-ylabel('$z$','Interpreter','Latex')
 xlabel('$u$,$v$','Interpreter','Latex')
 xlim([0 12])
-title('Evolution des profils')
+title('Solution stationnaire de u et v dans le plan u,v')
 grid on
 hold off
 
@@ -167,11 +167,11 @@ hold on
 plot(u_t,v_t)
 xlabel('u')
 ylabel('v')
-title('u fonction de v')
+title('Solutions transitoire de u et v dans le plan u,v')
 grid on
 hold off
 
-%unable to trace fig(6) too much info, maybe down nptz 
+%unable to trace fig(6) too much info, maybe down nptz
 %u_t(z),v_t(z)
 figure(6)
 hold on
@@ -180,27 +180,30 @@ plot(v_t,znoeuds)
 ylabel('$z$','Interpreter','Latex')
 xlabel('$u$,$v$','Interpreter','Latex')
 legend('u','v')
-title('Evolution des profils pour différents temps')
+title('Evolution des profils transitoire pour differents temps')
 grid on
 hold off
 
 %plot differents pas de temps
 figure(7)
 hold on
-for i=1:1000:npastemps
-    plot(u_t(i,:),znoeuds)
-    plot(v_t(i,:),znoeuds)
-end    
+plot(u_l,znoeuds,'m')
+plot(v_l,znoeuds,'k')
+for i=1:2000:npastemps
+    plot(u_t(i,:),znoeuds,'g')
+    plot(v_t(i,:),znoeuds,'r')
+end
 ylabel('$z$','Interpreter','Latex')
 xlabel('$u$,$v$','Interpreter','Latex')
-legend('u','v')
-title('Evolution des profils pour différents temps')
+legend('u','v','u(t)','v(t)')
+title('solutions stationnaire et regime transitoire pour differents temps')
 grid on
 hold off
-
+%-------------------------------
 %Sauvegarde des figures
-path = pwd ;   % mention your path 
-myfolder = 'graphe' ;   % new folder name 
+%-------------------------------
+path = pwd ;   % mention your path
+myfolder = 'graphe' ;   % new folder name
 folder = mkdir([path,filesep,myfolder]) ;
 path  = [path,filesep,myfolder] ;
 for i = 1:7
@@ -221,5 +224,3 @@ v(nptz)=0;
 % v(1)=0;
 % v(nptz)=0;
 end
-
-
